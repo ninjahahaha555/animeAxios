ffe1cfa857459b09ddab0c2b54b1b338e5e4613f
Thanawat Samkeam <ninjahahaha555@users.noreply.github.com>
2020-09-25 17:04:20 +0700

    four

--- src/components/Anime.vue
+++ src/components/Anime.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="row text-light">
+  <div class="'row text-light">
     <div class="col-sm"></div>
 
     <div class="col-sm">
       <input type="text" v-model="textSearch" placeholder="ค้นหาการ์ตูน" />
-      <b-button @click="searchData()" variant="danger">Search Anime</b-button><br/><br/>
+      <b-button @click="searchData()" variant="danger">Search Anime</b-button
+      ><br /><br />
       <b-card-group columns>
         <b-card
-          v-for="data in animeData"
+          v-for="data in animeData.slice(
+            (currentPage - 1) * perPage,
+            (currentPage - 1) * perPage + perPage
+          )"
           :key="data.mal_id"
           style="width: 450px"
           class="mb-3"
@@ -27,6 +31,18 @@
           </b-row>
         </b-card>
       </b-card-group>
+
+      <div v-if="run == true">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="animeData.length"
+          :per-page="perPage"
+          first-text="First"
+          prev-text="Prev"
+          next-text="Next"
+          last-text="Last"
+        ></b-pagination>
+      </div>
     </div>
 
     <div class="col-sm"></div>
@@ -38,8 +54,11 @@ import axios from "axios";
 export default {
   data() {
     return {
-      animeData: null,
+      animeData: "",
       textSearch: "",
+      perPage: 9,
+      currentPage: 1,
+      run: false,
     };
   },
   methods: {
@@ -47,7 +66,7 @@ export default {
       axios
         .get("https://api.jikan.moe/v3/search/anime?q=" + this.textSearch + "")
         .then((response) => {
-          this.animeData = response.data.results.slice(0, 15);
+          (this.animeData = response.data.results), (this.run = true);
         })
         .catch((err) => {
           console.log(err);
